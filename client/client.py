d3393785106d99b8372083b1ce848c74d8f97e80
kangchong978 <kangchong978@gmail.com>
2023-07-01 03:29:25 +0800

    Update# progress indicator

--- client/client.py
+++ client/client.py
@@ -1,5 +1,7 @@
 import json
 import mysql.connector
+from tqdm import tqdm
+from PyQt5.QtWidgets import QApplication, QProgressDialog
 
 # Connection details for the remote database
 config = {
@@ -18,59 +20,59 @@ class Client:
     @staticmethod
     def setCursor(cursor):
         Client.cursor = cursor
-    
+
     @staticmethod
     def getPrograms():
         cursor.execute("SELECT * FROM programs")
         results = cursor.fetchall()
-        return Client.parseToDict(results)
-    
+        return Client.parseToDictWithProgress(results, 'Fetching Programs')
+
     @staticmethod
     def getUsers():
         cursor.execute("SELECT * FROM users")
         results = cursor.fetchall()
-        return Client.parseToDict(results)
-    
+        return Client.parseToDictWithProgress(results, 'Fetching Users')
+
     @staticmethod
     def getNotifications():
         cursor.execute("SELECT * FROM notifications")
         results = cursor.fetchall()
-        return Client.parseToDict(results)
+        return Client.parseToDictWithProgress(results, 'Fetching Notifications')
 
     @staticmethod
     def getAccount():
         # latest 1 only
         cursor.execute("SELECT * FROM company ORDER BY updatedTimestamp DESC LIMIT 1")
         results = cursor.fetchall()
-        parsed = Client.parseToDict(results)
+        parsed = Client.parseToDictWithProgress(results, 'Fetching Account')
         if len(parsed) > 0:
             return parsed[0]
-            pass
         return None
+
     @staticmethod
     def getApproval():
         cursor.execute("SELECT * FROM approval")
         results = cursor.fetchall()
-        return Client.parseToDict(results)
-    
+        return Client.parseToDictWithProgress(results, 'Fetching Approval')
+
     @staticmethod
     def getDepartment():
         cursor.execute("SELECT * FROM department")
         results = cursor.fetchall()
-        return Client.parseToDict(results)
-    
+        return Client.parseToDictWithProgress(results, 'Fetching Departments')
+
     @staticmethod
     def getFileStorage():
         cursor.execute("SELECT * FROM files_storage")
         results = cursor.fetchall()
-        return Client.parseToDict(results)
+        return Client.parseToDictWithProgress(results, 'Fetching File Storage')
 
     @staticmethod
     def getCompany():
         cursor.execute("SELECT * FROM company")
         results = cursor.fetchall()
-        return Client.parseToDict(results)
-    
+        return Client.parseToDictWithProgress(results, 'Fetching Company')
+
     @staticmethod
     def parseToDict(data):
         result_list = []
@@ -81,41 +83,65 @@ class Client:
             result_list.append(result_dict)
 
         return result_list
-    
+
+    @staticmethod
+    def parseToDictWithProgress(data, desc):
+        result_list = []
+        columns = [column[0] for column in cursor.description]  # Get the column names
+
+        progress_dialog = QProgressDialog(desc, None, 0, len(data))
+        progress_dialog.setWindowModality(1)  # Set the dialog to modal
+        progress_dialog.setWindowTitle("Progress")
+        progress_dialog.show()
+
+        for i, row in enumerate(data):
+            result_dict = dict(zip(columns, row))
+            result_list.append(result_dict)
+            progress_dialog.setValue(i + 1)
+            QApplication.processEvents()  # Process events to keep GUI responsive
+
+
+        return result_list
+
+    @staticmethod
+    def executeWithProgress(query, values, desc):
+        with tqdm(total=1, desc=desc) as pbar:
+            cursor.execute(query, values)
+            connection.commit()
+            pbar.update(1)
+
     @staticmethod
     def updateAccount(accountData):
         print(f"{accountData}")
-        query = "INSERT INTO company ( amount, updatedTimestamp ) VALUES (%s, %s)"
+        query = "INSERT INTO company (amount, updatedTimestamp) VALUES (%s, %s)"
         values = (accountData['amount'], accountData['updatedTimestamp'])
 
-        cursor.execute(query, values)
-        connection.commit()
+        Client.executeWithProgress(query, values, 'Updating Account')
 
         if cursor.rowcount > 0:
             return True
         else:
             return False
-        
+
     @staticmethod
-    def updateUserProgramApprovements(id,programsData):
+    def updateUserProgramApprovements(id, programsData):
         query = "UPDATE users SET programs = %s WHERE id = %s"
         values = (f'{{"programs":{json.dumps(programsData)}}}', id)
 
-        cursor.execute(query, values)
-        connection.commit()
+        Client.executeWithProgress(query, values, 'Updating User Program Approvements')
 
         if cursor.rowcount > 0:
             return True
         else:
             return False
 
+    @staticmethod
     def updateApproval(approvalData):
         print(f"{approvalData}")
         query = "UPDATE approval SET approveStatus = %s WHERE id = %s"
         values = (approvalData['approveStatus'], approvalData['id'])
 
-        cursor.execute(query, values)
-        connection.commit()
+        Client.executeWithProgress(query, values, 'Updating Approval')
 
         if cursor.rowcount > 0:
             return True
@@ -127,8 +153,7 @@ class Client:
         query = "INSERT INTO users (id, username, departmentId) VALUES (%s, %s, %s)"
         values = (userData['id'], userData['username'], userData['departmentId'])
 
-        cursor.execute(query, values)
-        connection.commit()
+        Client.executeWithProgress(query, values, 'Inserting User')
 
         if cursor.rowcount > 0:
             return True
