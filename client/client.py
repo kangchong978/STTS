5c289920c86814a7966c77e8796abcc714bc6632
kangchong978 <kangchong978@gmail.com>
2023-06-30 16:51:37 +0800

    Update# db connections

--- client/client.py
+++ client/client.py
@@ -1,18 +1,73 @@
 import json
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host='localhost',
+    port=3306,
+    user='stts',
+    database='stts_db'
+)
+cursor = connection.cursor()
 
 class Client:
-    def __init__(self):
-        pass
+    @staticmethod
+    def setCursor(cursor):
+        Client.cursor = cursor
     
+    @staticmethod
     def getPrograms():
-        return json.load(open('dummyData.json'))['programs']
+        cursor.execute("SELECT * FROM programs")
+        results = cursor.fetchall()
+        return Client.parseToDict(results)
     
+    @staticmethod
     def getUsers():
-        return json.load(open('dummyDataUser.json'))['users']
+        cursor.execute("SELECT * FROM users")
+        results = cursor.fetchall()
+        return Client.parseToDict(results)
     
+    @staticmethod
     def getNotifications():
-        return json.load(open('dummyNotifications.json'))['notifications']
+        cursor.execute("SELECT * FROM notifications")
+        results = cursor.fetchall()
+        return Client.parseToDict(results)
 
+    @staticmethod
     def getAccount():
-        return json.load(open('dummyAccounts.json'))['account']
+        cursor.execute("SELECT * FROM account")
+        results = cursor.fetchall()
+        return Client.parseToDict(results)
+    @staticmethod
+    def getPrograms():
+        cursor.execute("SELECT * FROM approval")
+        results = cursor.fetchall()
+        return Client.parseToDict(results)
     
+    @staticmethod
+    def getUsers():
+        cursor.execute("SELECT * FROM department")
+        results = cursor.fetchall()
+        return Client.parseToDict(results)
+    
+    @staticmethod
+    def getNotifications():
+        cursor.execute("SELECT * FROM files_storage")
+        results = cursor.fetchall()
+        return Client.parseToDict(results)
+
+    @staticmethod
+    def getAccount():
+        cursor.execute("SELECT * FROM company")
+        results = cursor.fetchall()
+        return Client.parseToDict(results)
+    
+    @staticmethod
+    def parseToDict(data):
+        result_list = []
+        columns = [column[0] for column in cursor.description]  # Get the column names
+
+        for row in data:
+            result_dict = dict(zip(columns, row))
+            result_list.append(result_dict)
+
+        return result_list
